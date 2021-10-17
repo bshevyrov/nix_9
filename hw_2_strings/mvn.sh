5bb05bf76231fac25f315e3ad13a86d4741b0dc2
bohdan <bshevyrov@gmail.com>
2021-10-17 22:55:22 +0300

    Start hw_2_strings

---
+++ hw_2_strings/mvn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn clean install
+java -jar target/hw_2_strings.jar
---
+++ hw_2_strings/src/main/java/ua/com/alevel/StartApp.java
@@ -0,0 +1,7 @@
+package ua.com.alevel;
+
+public class StartApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
