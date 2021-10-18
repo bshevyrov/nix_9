339fa78d3fb7f137a72163023975b658179e7501
bohdan <bshevyrov@gmail.com>
2021-10-18 03:38:30 +0300

    Second method done

--- hw_2_strings/src/main/java/ua/com/alevel/Logic.java
+++ hw_2_strings/src/main/java/ua/com/alevel/Logic.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Logic {
     public static String reverse(String src) {
         StringBuilder stringBuilder = new StringBuilder();
+
         String[] words = stringToWords(src);
         for (int i = 0; i < words.length; i++) {
             words[i] = wordRevers(words[i]);
@@ -15,18 +16,31 @@ public class Logic {
                 stringBuilder.append(" ");
             }
         }
-
-
         return String.valueOf(stringBuilder);
     }
 
     public static String reverse(String src, String dest) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
 
+        String[] words = stringToWords(src);
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains(dest)) {
+                words[i] = wordRevers(words[i], words[i].lastIndexOf(dest),
+                        words[i].lastIndexOf(dest) + dest.length());
+            }
+            stringBuilder.append(words[i]);
+            if (i != words.length - 1) {
+                stringBuilder.append(" ");
+            }
+        }
+        return String.valueOf(stringBuilder);
     }
 
     public static String reverse(String src, int firstIndex, int lastIndex) {
-        return "";
+
+
+
+        return wordRevers(src, firstIndex, lastIndex);
 
     }
 
@@ -53,6 +67,28 @@ public class Logic {
             str[i] = str[str.length - (i + 1)];
             str[str.length - (i + 1)] = tmpSwap;
         }
+        return String.copyValueOf(str);
+    }
+
+    private static String wordRevers(String string, int start, int finish) {
+        char tmpSwap;
+        char[] str = string.toCharArray();
+        int j = 1;
+        if (((start + finish) / 2) > 1) {
+            for (int i = start; i < start + (start + finish / 2); i++) {
+
+                tmpSwap = str[i];
+                str[i] = str[finish - j];
+                str[finish - j] = tmpSwap;
+                j++;
+            }
+        } else {
+            tmpSwap = str[start];
+            str[start] = str[finish - j];
+            str[finish - j] = tmpSwap;
+        }
+
+
         return String.copyValueOf(str);
     }
 }
--- hw_2_strings/src/main/java/ua/com/alevel/StartApp.java
+++ hw_2_strings/src/main/java/ua/com/alevel/StartApp.java
@@ -2,6 +2,8 @@ package ua.com.alevel;
 
 public class StartApp {
     public static void main(String[] args) {
-        System.out.println( Logic.reverse("hello world"));
+        System.out.println(Logic.reverse("hello world"));
+        System.out.println(Logic.reverse("hello world", "worl"));
+        System.out.println(Logic.reverse("hello world", 3, 7));
     }
 }
