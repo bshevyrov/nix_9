f02cda629fbbb531750f53761b3b2756627d95ec
bohdan <bshevyrov@gmail.com>
2021-10-22 23:25:06 +0300

    Exctract exit task in new method

--- console_helper/src/main/java/ua/com/alevel/ScreenMenu.java
+++ console_helper/src/main/java/ua/com/alevel/ScreenMenu.java
@@ -11,8 +11,6 @@ public class ScreenMenu {
     }
 
     public static void run(ArrayList<TaskHelper> taskHelpers, ArrayList<String> descriptions) throws IOException {
-       addExitTaskAndDescription(taskHelpers, descriptions);
-
         System.out.println("MENU: ");
         for (int i = 0; i < taskHelpers.size(); i++) {
             System.out.println(i + ". " + descriptions.get(i));
@@ -22,7 +20,7 @@ public class ScreenMenu {
         taskHelpers.get(input).run(bufferedReader);
 
     }
-    private static void addExitTaskAndDescription(ArrayList<TaskHelper> taskHelpers, ArrayList<String> descriptions) {
+    public static void addExitTaskAndDescription(ArrayList<TaskHelper> taskHelpers, ArrayList<String> descriptions) {
         descriptions.add(descriptions.size(), "Press me if you want to exit");
         taskHelpers.add(taskHelpers.size() , reader -> {
             System.out.println("Thank You and Good Bye");
