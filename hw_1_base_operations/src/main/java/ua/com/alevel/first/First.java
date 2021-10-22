79c1df59d35aff219468b499dd896999b4cfc4f8
bohdan <bshevyrov@gmail.com>
2021-10-22 17:48:51 +0300

    add input verification

--- hw_1_base_operations/src/main/java/ua/com/alevel/first/First.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/first/First.java
@@ -1,5 +1,6 @@
 package ua.com.alevel.first;
 
+import org.apache.commons.lang3.StringUtils;
 import ua.com.alevel.TaskHelper;
 
 import java.io.BufferedReader;
@@ -11,20 +12,27 @@ public class First implements TaskHelper {
     public void run(BufferedReader bufferedReader) {
 
         int sum = 0;
-        char[] symbols = new char[0];
-
+        char[] symbols;
+        String inputString;
         System.out.print("Please enter a string: ");
         try {
-            symbols = bufferedReader.readLine().toCharArray();
+            inputString = bufferedReader.readLine();
+            if (StringUtils.isNotBlank(inputString) && StringUtils.isNumericSpace(inputString)) {
+                symbols = inputString.toCharArray();
+                for (char ch : symbols) {
+                    if (Character.isDigit(ch)) {
+                        sum += Character.getNumericValue(ch);
+                    }
+                }
+                System.out.println("Sum of numbers are: " + sum);
+            } else {
+                System.out.println("Input wrong, please try again..");
+                run(bufferedReader);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (char ch : symbols) {
-            if (Character.isDigit(ch)) {
-                sum += Character.getNumericValue(ch);
-            }
-        }
-        System.out.println("Sum of numbers are: " + sum);
+
     }
 
 
