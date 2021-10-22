01636a15aaa61031a685aa8ad34f76e7eeb7837d
bohdan <bshevyrov@gmail.com>
2021-10-23 02:45:34 +0300

    refactoring ThirdMethodStarter

--- hw_2_strings/src/main/java/ua/com/alevel/helper/ThirdMethodStarter.java
+++ hw_2_strings/src/main/java/ua/com/alevel/helper/ThirdMethodStarter.java
@@ -1,18 +1,58 @@
 package ua.com.alevel.helper;
 
+import org.apache.commons.lang3.StringUtils;
 import ua.com.alevel.StringHelperUtil;
 import ua.com.alevel.TaskHelper;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class ThirdMethodStarter implements TaskHelper {
+
     @Override
     public void run(BufferedReader reader) {
+        String str = "";
+        String firstNum = "";
+        String secondNum = "";
+        while (true) {
+            System.out.print("Write string: ");
+            try {
+                str = reader.readLine();
 
-        System.out.print("Write words and separate by spaces two numbers: ");
-        String[] compoundArguments = ArgumentsCompounded.getStingAndTwoNum(reader);
-        String rsl = StringHelperUtil.reverse(compoundArguments[0],
-                Integer.parseInt(compoundArguments[1]),
-                Integer.parseInt(compoundArguments[2]));
+                if (StringUtils.isBlank(str)) {
+                    System.out.println("String is not valid. Try again");
+                    continue;
+                }
+                System.out.print("Write first number: ");
+
+                firstNum = reader.readLine();
+
+                if (!StringUtils.isNumeric(firstNum)) {
+                    System.out.println("Its not number. Try again");
+                    continue;
+                }
+                if (Integer.parseInt(firstNum) < 0 && Integer.parseInt(firstNum) >= str.length()) {
+                    System.out.println("Abnormal fist number. Try again");
+                    continue;
+                }
+                System.out.print("Writer second number: ");
+
+                secondNum = reader.readLine();
+                if (!StringUtils.isNumeric(secondNum)) {
+                    System.out.println("Its not number. Try again");
+                    continue;
+                }
+                if (Integer.parseInt(secondNum) < Integer.parseInt(firstNum) &&
+                        Integer.parseInt(secondNum) >= str.length()) {
+                    System.out.println("Abnormal second number. Try again");
+                    continue;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            break;
+        }
+        String rsl = StringHelperUtil.reverse(str, Integer.parseInt(firstNum), Integer.parseInt(secondNum));
         System.out.println("Result is: " + rsl);
     }
 }
