a84a06c7f39633279249a334d1d8d20cf4d39403
bohdan <bshevyrov@gmail.com>
2021-11-02 02:08:30 +0200

    finish logic of chess task

--- module_1/src/main/java/ua/com/alevel/StartApp.java
+++ module_1/src/main/java/ua/com/alevel/StartApp.java
@@ -4,13 +4,18 @@ import ua.com.alevel.firstlevel.AreaOfATriangle;
 import ua.com.alevel.firstlevel.ArrayUtil;
 import ua.com.alevel.firstlevel.KnightMove;
 
+import java.util.Arrays;
+
 public class StartApp {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
 
         //new ArrayUtil().getAmountOfUniqNumber(new int[]{1, 4, 5, 1, 1, 3});
        // new AreaOfATriangle().getArea(new int[][]{{1, 3}, {2, -5}, {-8, 4}});
-   // new KnightMove().generateField();
-        System.out.println( new KnightMove().findCellByCords('H',8));
+   new KnightMove().generateField();
+   new KnightMove().addStartPointToChessField(4,'D');
+   new KnightMove().addFinishPointToChessField(5,'B');
+
+       new KnightMove().checkMove(4,'D',5,'B');
     }
 }
--- module_1/src/main/java/ua/com/alevel/firstlevel/KnightMove.java
+++ module_1/src/main/java/ua/com/alevel/firstlevel/KnightMove.java
@@ -7,51 +7,70 @@ import java.util.Arrays;
 public class KnightMove {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-    String[] a = new String[65];
+    private static final String START_POINT_ICON = "♞";
+    private static final String FINISH_POINT_ICON = "♘";
+   static String[][] cell = new String[9][9];
 
 
-    public void generateField(String[] cells) {
-        if (cells[cells.length - 1] == null) {
-            Arrays.fill(cells, " ");
+    public void generateField() {
+        if (cell[cell.length - 1][cell.length - 1] == null) {
+            for (String[] strings : cell) {
+                Arrays.fill(strings, " ");
+            }
+           // Arrays.fill(cells, " ");
         }
         System.out.println("♞");
         System.out.println("♘");
-        System.out.println("");
+        System.out.println();
         System.out.println("         A   B   C   D   E   F   G   H");
-        System.out.println("");
+        System.out.println();
         System.out.println("       ╔═══════════════════════════════╗");
-        System.out.println("    8  ║ " + a[1] + " │ " + a[2] + " │ " + a[3] + " │ " + a[4] + " │ " + a[5] + " │ " + a[6] + " │ " + a[7] + " │ " + a[8] + " ║  8   ");
+        System.out.println("    8  ║ " + cell[8][1] + " │ " + cell[8][2] + " │ " + cell[8][4] + " │ " + cell[8][4] + " │ " + cell[8][5] + " │ " + cell[8][6]+ " │ " + cell[8][7] + " │ " + cell[8][8] + " ║  8   ");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    7  ║ " + a[9] + " │ " + a[10] + " │ " + a[11] + " │ " + a[12] + " │ " + a[13] + " │ " + a[14] + " │ " + a[15] + " │ " + a[16] + " ║  7");
+        System.out.println("    7  ║ " + cell[7][1] + " │ " + cell[7][2] + " │ " + cell[7][3] + " │ " + cell[7][4] + " │ " + cell[7][5] + " │ " + cell[7][6] + " │ " + cell[7][7] + " │ " + cell[7][8] + " ║  7");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    6  ║ " + a[17] + " │ " + a[18] + " │ " + a[19] + " │ " + a[20] + " │ " + a[21] + " │ " + a[22] + " │ " + a[23] + " │ " + a[24] + " ║  6");
+        System.out.println("    6  ║ " + cell[6][1] + " │ " + cell[6][2] + " │ " + cell[6][3] + " │ " + cell[6][4] + " │ " + cell[6][5] + " │ " + cell[6][6] + " │ " + cell[6][7] + " │ " + cell[6][8] + " ║  6");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    5  ║ " + a[25] + " │ " + a[26] + " │ " + a[27] + " │ " + a[28] + " │ " + a[29] + " │ " + a[30] + " │ " + a[31] + " │ " + a[32] + " ║  5");
+        System.out.println("    5  ║ " + cell[5][1] + " │ " + cell[5][2] + " │ " + cell[5][3] + " │ " + cell[5][4] + " │ " + cell[5][5] + " │ " + cell[5][6] + " │ " + cell[5][7] + " │ " + cell[5][8] + " ║  5");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    4  ║ " + a[33] + " │ " + a[34] + " │ " + a[35] + " │ " + a[36] + " │ " + a[37] + " │ " + a[38] + " │ " + a[39] + " │ " + a[40] + " ║  4");
+        System.out.println("    4  ║ " + cell[4][1] + " │ " + cell[4][2] + " │ " + cell[4][3] + " │ " + cell[4][4] + " │ " + cell[4][5] + " │ " + cell[4][6] + " │ " + cell[4][7] + " │ " + cell[4][8] + " ║  4");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    3  ║ " + a[41] + " │ " + a[42] + " │ " + a[43] + " │ " + a[44] + " │ " + a[45] + " │ " + a[46] + " │ " + a[47] + " │ " + a[48] + " ║  3");
+        System.out.println("    3  ║ " + cell[3][1] + " │ " + cell[3][2] + " │ " + cell[3][3] + " │ " + cell[3][4] + " │ " + cell[3][5] + " │ " + cell[3][6] + " │ " + cell[3][7] + " │ " + cell[3][8] + " ║  3");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    2  ║ " + a[49] + " │ " + a[50] + " │ " + a[51] + " │ " + a[52] + " │ " + a[53] + " │ " + a[54] + " │ " + a[55] + " │ " + a[56] + " ║  2");
+        System.out.println("    2  ║ " + cell[2][1] + " │ " + cell[2][2] + " │ " + cell[2][3] + " │ " + cell[2][4] + " │ " + cell[2][5] + " │ " + cell[2][6] + " │ " + cell[2][7] + " │ " + cell[2][8] + " ║  2");
         System.out.println("       ╟───┼───┼───┼───┼───┼───┼───┼───╢");
-        System.out.println("    1  ║ " + a[57] + " │ " + a[58] + " │ " + a[59] + " │ " + a[60] + " │ " + a[61] + " │ " + a[62] + " │ " + a[63] + " │ " + a[64] + " ║  1");
+        System.out.println("    1  ║ " + cell[1][1] + " │ " + cell[1][2] + " │ " + cell[1][3] + " │ " + cell[1][4] + " │ " + cell[1][5] + " │ " + cell[1][6] + " │ " + cell[1][7] + " │ " + cell[1][8] + " ║  1");
         System.out.println("       ╚═══════════════════════════════╝");
-        System.out.println("");
+        System.out.println();
         System.out.println("         A   B   C   D   E   F   G   H");
 
     }
 
-    public int findCellByCords(char letter, int num) {
-        return getNumFromLetter(letter) * (9 - (num));
-    }
 
+//    knigth move +-1 +-2 int char
+//    if (start point + knight move) in chess feld && legit move for knight
+public void checkMove(int numStart, char letterStart,int numFinish,char letterFinish){
+
+    if((Math.abs(numFinish-numStart)==1)&&(Math.abs(getNumFromLetter(letterFinish)-getNumFromLetter(letterStart))==2)){
+        System.out.println("GOOD Move");
+        generateField();
+    } else {
+        System.out.println("Sorry illigal move");
+    }
+}
+
+ public void addStartPointToChessField(int num, char letter){
+        cell[num][getNumFromLetter(letter)]=START_POINT_ICON;
+ }
+ public void addFinishPointToChessField(int num, char letter){
+        cell[num][getNumFromLetter(letter)]=FINISH_POINT_ICON;
+ }
     private int getNumFromLetter(char letter) {
         String[] lettersSynopsis = new String[]{" ", "A", "B", "C", "D", "E", "F", "G", "H"};
 
         return ArrayUtils.indexOf(
                 lettersSynopsis, Character.toString(letter));
     }
-public void isPossibleTurn(char letter, int num){
 
 }
-}
+
