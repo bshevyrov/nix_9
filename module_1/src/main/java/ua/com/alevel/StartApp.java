9c021261ddf834b06b7cf06378b2420f5f32477f
bohdan <bshevyrov@gmail.com>
2021-11-02 21:49:05 +0200

    string verification task complete

--- module_1/src/main/java/ua/com/alevel/StartApp.java
+++ module_1/src/main/java/ua/com/alevel/StartApp.java
@@ -1,10 +1,6 @@
 package ua.com.alevel;
 
-import ua.com.alevel.firstlevel.AreaOfATriangle;
-import ua.com.alevel.firstlevel.ArrayUtil;
-import ua.com.alevel.firstlevel.KnightMove;
-
-import java.util.Arrays;
+import ua.com.alevel.secondlevel.StringVerification;
 
 public class StartApp {
     public static void main(String[] args) {
@@ -12,10 +8,14 @@ public class StartApp {
 
         //new ArrayUtil().getAmountOfUniqNumber(new int[]{1, 4, 5, 1, 1, 3});
        // new AreaOfATriangle().getArea(new int[][]{{1, 3}, {2, -5}, {-8, 4}});
-   new KnightMove().generateField();
-   new KnightMove().addStartPointToChessField(4,'D');
-   new KnightMove().addFinishPointToChessField(5,'B');
 
-       new KnightMove().checkMove(4,'D',5,'B');
+
+        //
+        //        new KnightMove().generateField();
+        //        new KnightMove().addStartPointToChessField(4,'D');
+        //        new KnightMove().addFinishPointToChessField(5,'B');
+        //        new KnightMove().checkMove(4,'D',9,'B');
+       boolean rsl = new StringVerification().stringVerification("(");
+        System.out.println(rsl?"This string pass verification.":"Sorry this string have problem");
     }
 }
---
+++ module_1/src/main/java/ua/com/alevel/secondlevel/StringVerification.java
@@ -0,0 +1,69 @@
+package ua.com.alevel.secondlevel;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringVerification {
+
+    public boolean stringVerification(String str) {
+        str = cleanString(str);
+        if (str.length() == 0) {
+            return true;
+        }
+        if (isAllNeededSymbolsHavePair(str)) {
+            for (int i = 0; i < (str.length()) / 2; i++) {
+                if (str.charAt(i) != str.charAt((str.length()) / 2)) {
+                       return false;
+
+                }
+            }
+        } else {
+               return false;
+        }
+        System.out.println(str);
+        return true;
+    }
+
+    private String cleanString(String str) {
+        str = deleteAllOtherSymbols(str);
+        str = removeSmallPair(str);
+        str = deleteAllOtherSymbols(str);
+        return str;
+    }
+
+    private boolean isAllNeededSymbolsHavePair(String str) {
+        return StringUtils.countMatches(str, '{') == StringUtils.countMatches(str, '}')
+                && (StringUtils.countMatches(str, '(') == StringUtils.countMatches(str, ')'))
+                && (StringUtils.countMatches(str, '[') == StringUtils.countMatches(str, ']'));
+    }
+
+    private String deleteAllOtherSymbols(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '{' || str.charAt(i) == '}'
+                    || str.charAt(i) == '(' || str.charAt(i) == ')'
+                    || str.charAt(i) == '[' || str.charAt(i) == ']') {
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private boolean isProtagonist(char first, char second) {
+        return ((first == '{') && (second == '}'))
+                || (((first == '(') && (second == ')')))
+                || (((first == '[') && (second == ']')));
+    }
+
+    private String removeSmallPair(String str) {
+        char[] string = str.toCharArray();
+
+        for (int i = 0; i < string.length - 1; i++) {
+            if (isProtagonist(string[i], string[i + 1])) {
+                string[i] = ' ';
+                string[i + 1] = ' ';
+                i++;
+            }
+        }
+        return new String(string);
+    }
+}
