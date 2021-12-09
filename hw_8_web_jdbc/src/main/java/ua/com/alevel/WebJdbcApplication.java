26459fc6ecd6e52da68afcd4bdf314e4d7850df1
bohdan <bshevyrov@gmail.com>
2021-12-10 00:35:26 +0200

    init commit

---
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/WebJdbcApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebJdbcApplication.class, args);
+    }
+
+}
