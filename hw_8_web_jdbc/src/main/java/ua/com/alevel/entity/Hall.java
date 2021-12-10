057d3a87447ccae622341f8fbc2851abba8f6f91
bohdan <bshevyrov@gmail.com>
2021-12-10 02:42:41 +0200

    create entity layer

---
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/entity/Hall.java
@@ -0,0 +1,36 @@
+package ua.com.alevel.entity;
+
+public class Hall extends BaseEntity {
+
+    private String name;
+    private int numOfSeats;
+    private Movie movie;
+
+    public Hall() {
+        super();
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(int numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+}
---
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/entity/Movie.java
@@ -0,0 +1,27 @@
+package ua.com.alevel.entity;
+
+public class Movie extends BaseEntity {
+
+    private String name;
+    private String description;
+
+    public Movie() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
