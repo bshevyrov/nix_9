59dc1282636e7637daf5fe5080ef90342b271eb0
bohdan <bshevyrov@gmail.com>
2021-12-13 05:19:03 +0200

    create some hall controller and create some web hall pages and create css, layout

--- hw_8_web_jdbc/src/main/java/ua/com/alevel/controller/HallController.java
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/controller/HallController.java
@@ -3,7 +3,10 @@ package ua.com.alevel.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.com.alevel.dto.hall.HallRequestDto;
 import ua.com.alevel.dto.hall.HallResponseDto;
 import ua.com.alevel.facade.HallFacade;
 
@@ -18,11 +21,23 @@ public class HallController {
     public HallController(HallFacade hallFacade) {
         this.hallFacade = hallFacade;
     }
-
-@GetMapping
-    public String findAll(Model model){
+    @GetMapping
+    public String findAll(Model model) {
         List<HallResponseDto> halls = hallFacade.findAll();
         model.addAttribute("halls", halls);
         return "pages/hall/hall_all";
+    }
+
+    @GetMapping("/new")
+    public String redirectToNewHallPage(Model model) {
+        //модел  адд атрибьют, те модели что передаем на ХТМЛИНУ
+        model.addAttribute("hall", new HallRequestDto());
+        return "pages/hall/hall_new";
+    }
+
+    @PostMapping("/new")
+    public String CreateNewHall(@ModelAttribute("hall") HallRequestDto hallRequestDto) {
+        hallFacade.create(hallRequestDto);
+        return "redirect:/halls";
     }
 }
