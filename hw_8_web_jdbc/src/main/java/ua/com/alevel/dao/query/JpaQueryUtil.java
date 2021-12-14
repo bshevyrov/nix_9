2f6bfa85a374f18de64352a87770523bb50a766a
bohdan <bshevyrov@gmail.com>
2021-12-14 05:46:59 +0200

    JpaQueryUtil all hall query

--- hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/query/JpaQueryUtil.java
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/query/JpaQueryUtil.java
@@ -5,8 +5,9 @@ public final class JpaQueryUtil {
     private JpaQueryUtil(){}
 
     public static final String CREATE_HALL_QUERY="INSERT INTO halls VALUES(default,?,?)";
-    public static final String UPDATE_HALL_QUERY="INSERT INTO halls VALUES(default,?,?)";
+    public static final String UPDATE_HALL_QUERY="UPDATE halls SET name = ?, num_of_seat = ? WHERE id = ?";
     public static final String DELETE_HALL_BY_ID_QUERY="DELETE FROM halls WHERE id = ";
-    public static final String FIND_BY_ID_QUERY="INSERT INTO halls VALUES(default,?,?)";
+//    public static final String FIND_BY_ID_QUERY="INSERT INTO halls VALUES(default,?,?)";
     public static final String FIND_ALL_HALL_QUERY="SELECT * FROM halls";
+    public static final String EXIST_HALL_BY_ID_QUERY="SELECT COUNT(*) FROM students WHERE id = ";
 }
