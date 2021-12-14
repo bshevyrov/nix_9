594a95caf3bdfd3ca57473d37c74c8409968b1a9
bohdan <bshevyrov@gmail.com>
2021-12-14 21:55:59 +0200

    reformat JpaQueryUtil

--- hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/query/JpaQueryUtil.java
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/query/JpaQueryUtil.java
@@ -7,7 +7,6 @@ public final class JpaQueryUtil {
     public static final String CREATE_HALL_QUERY="INSERT INTO halls VALUES(default,?,?)";
     public static final String UPDATE_HALL_QUERY="UPDATE halls SET name = ?, num_of_seat = ? WHERE id = ?";
     public static final String DELETE_HALL_BY_ID_QUERY="DELETE FROM halls WHERE id = ";
-//    public static final String FIND_BY_ID_QUERY="INSERT INTO halls VALUES(default,?,?)";
     public static final String FIND_ALL_HALL_QUERY="SELECT * FROM halls";
     public static final String EXIST_HALL_BY_ID_QUERY="SELECT COUNT(*) FROM students WHERE id = ";
 
@@ -15,9 +14,6 @@ public final class JpaQueryUtil {
     public static final String UPDATE_MOVIE_QUERY="UPDATE movies SET name = ?, description = ?, hall_id = ? WHERE id =";
     public static final String DELETE_MOVIE_BY_ID_QUERY="DELETE FROM movies WHERE id = ";
     public static final String DELETE_MOVIE_BY_HALL_ID_QUERY="DELETE FROM movies WHERE hall_id = ";
-
-//    public static final String FIND_ALL_HALL_QUERY="SELECT * FROM halls";
-// Чего не так, например
     public static final String FIND_ALL_MOVIE_QUERY="SELECT * FROM  movies AS m JOIN halls AS h ON  m.hall_id = h.id";
     public static final String FIND_ALL_MOVIE_BY_HALL_ID_QUERY="SELECT * FROM  movies AS m JOIN halls AS h ON  m.hall_id = h.id WHERE h.id = ";
     public static final String EXIST_MOVIE_BY_ID_QUERY="SELECT COUNT(*) FROM movies WHERE id = ";
