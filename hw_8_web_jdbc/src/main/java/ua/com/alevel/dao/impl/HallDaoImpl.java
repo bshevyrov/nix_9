934e28b523a949748d65aa3c7058b625f8d147f5
bohdan <bshevyrov@gmail.com>
2021-12-14 05:46:08 +0200

    HallDaoImpl all methods

--- hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/impl/HallDaoImpl.java
+++ hw_8_web_jdbc/src/main/java/ua/com/alevel/dao/impl/HallDaoImpl.java
@@ -35,6 +35,14 @@ public class HallDaoImpl implements HallDao {
 
     @Override
     public void update(Hall hall) {
+        try (PreparedStatement preparedStatement = jpaConfig.getConnection().prepareStatement(UPDATE_HALL_QUERY)) {
+            preparedStatement.setString(1, hall.getName());
+            preparedStatement.setInt(2, hall.getNumOfSeats());
+            preparedStatement.setLong(3, hall.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("SQL problem " + e.getMessage());
+        }
 
     }
 
@@ -49,12 +57,34 @@ public class HallDaoImpl implements HallDao {
 
     @Override
     public boolean existById(Long id) {
-        return false;
+        Long count = 0L;
+        try {
+            ResultSet resultSet = jpaConfig.getStatement().executeQuery(EXIST_HALL_BY_ID_QUERY + id);
+            while (resultSet.next()) {
+                count = resultSet.getLong("COUNT(*)");
+            }
+        } catch (SQLException throwables) {
+            System.out.println("SQL problem " + throwables.getMessage());
+        }
+        return count == 1;
     }
 
     @Override
     public Hall findById(Long id) {
-        return null;
+        // НОРМ??
+        Hall hall = new Hall();
+        if (existById(id)) {
+            try {
+                ResultSet resultSet = jpaConfig.getStatement().executeQuery(FIND_ALL_HALL_QUERY);
+                while (resultSet.next()) {
+                    hall = initHallByResultSet(resultSet);
+                }
+            } catch (SQLException e) {
+                System.out.println("Sql problem " + e.getMessage());
+            }
+        }
+        return hall;
+
     }
 
     @Override
