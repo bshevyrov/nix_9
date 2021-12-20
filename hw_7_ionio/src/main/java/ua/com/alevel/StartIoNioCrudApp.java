ea223fe295a8f0af3fbc481943155e6e9a734d0d
bohdan <bshevyrov@gmail.com>
2021-12-20 05:18:11 +0200

    refactor

--- hw_7_ionio/src/main/java/ua/com/alevel/StartIoNioCrudApp.java
+++ hw_7_ionio/src/main/java/ua/com/alevel/StartIoNioCrudApp.java
@@ -5,7 +5,6 @@ import ua.com.alevel.utils.CrudMenu;
 
 public class StartIoNioCrudApp {
     public static void main(String[] args) {
-//не работате создание авторов после книги, тоесть им не присваиваються ники и после создания книги при создании авторов им не присваивается бук айди
         new CrudMenu().run();
     }
 }
