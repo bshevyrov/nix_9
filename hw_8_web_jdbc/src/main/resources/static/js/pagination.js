2fc4b4af631ce1a69743de51ef7adf9457d1a0f4
bohdan <bshevyrov@gmail.com>
2021-12-23 23:15:06 +0200

    add pagination js

---
+++ hw_8_web_jdbc/src/main/resources/static/js/pagination.js
@@ -0,0 +1,50 @@
+function runPagination(page, size, paeOperator) {
+    let pageData = document.getElementById('pageData');
+    const sort = pageData.getAttribute('data-sort');
+    const order = pageData.getAttribute('data-order');
+    submitRequest(sort, order, page + pageOperator, size);
+}
+
+function runSort(sort, order) {
+    if (order === 'desc') {
+        order = 'asc';
+    } else {
+        order = 'desc';
+    }
+    let pageData = document.getElementById('pageData');
+    const page = pageData.getAttribute('data-page');
+    const size = pageData.getAttribute('data-size');
+    submitRequest(sort, order, page + pageOperator, size);
+
+    function submitRequest(sort, order, page, size) {
+        let personalSearchSubmit = document.getElementById('personalSearchSubmit');
+        if (personalSearchSubmit != null) {
+            let personalSearch = document.getElementById('personalSearch');
+            if (personalSearch != null) {
+                let input = document.createElement("input");
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", "sort");
+                input.setAttribute("value", sort);
+                personalSearch.appendChild(input);
+                input = document.createElement("input");
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", "sort");
+                input.setAttribute("value", sort);
+                personalSearch.appendChild(input);
+                input = document.createElement("input");
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", "sort");
+                input.setAttribute("value", sort);
+                personalSearch.appendChild(input);
+                input = document.createElement("input");
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", "sort");
+                input.setAttribute("value", sort);
+                personalSearch.appendChild(input);
+                personalSearchSubmit.click();
+
+            }
+        }
+
+    }
+}
