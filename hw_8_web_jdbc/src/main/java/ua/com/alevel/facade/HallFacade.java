8467df8342bdb4b93e95a459dbcba535c0a91105
bohdan <bshevyrov@gmail.com>
2022-01-02 19:36:44 +0200

    ah-shit-here-we-go-again

--- hw_8_web_jdbc/src/main/java/ua/com/alevel/facade/HallFacade.java
+++
@@ -1,7 +0,0 @@
-package ua.com.alevel.facade;
-
-import ua.com.alevel.view.dto.request.HallRequestDto;
-import ua.com.alevel.view.dto.response.HallResponseDto;
-
-public interface HallFacade extends BaseFacade<HallRequestDto, HallResponseDto>{
-}
