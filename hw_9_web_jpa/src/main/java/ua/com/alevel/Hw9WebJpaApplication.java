744982d2568e0018acf0c88a7bedbab2d27ba2af
bohdan <bshevyrov@gmail.com>
2022-01-17 05:26:53 +0200

    refactor

---
+++ hw_9_web_jpa/src/main/java/ua/com/alevel/Hw9WebJpaApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw9WebJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw9WebJpaApplication.class, args);
+    }
+
+}
