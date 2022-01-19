3fc28adc913128ed7e61cad3a88e10645cf0a15b
bohdan <bshevyrov@gmail.com>
2022-01-19 06:14:28 +0200

    initial commit

---
+++ hw_10_reposytory/src/main/java/ua/com/alevel/Hw10ReposytoryApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw10ReposytoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw10ReposytoryApplication.class, args);
+    }
+
+}
