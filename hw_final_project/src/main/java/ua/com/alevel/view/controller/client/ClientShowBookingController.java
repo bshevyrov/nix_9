db210fe230450be373dbe63baae3beaae4977d16
bohdan <bshevyrov@gmail.com>
2022-01-28 04:44:11 +0200

    booking page

---
+++ hw_final_project/src/main/java/ua/com/alevel/view/controller/client/ClientShowBookingController.java
@@ -0,0 +1,26 @@
+package ua.com.alevel.view.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.com.alevel.facade.ShowFacade;
+import ua.com.alevel.view.controller.AbstractController;
+
+@Controller
+@RequestMapping("/client/show")
+public class ClientShowBookingController extends AbstractController {
+
+    private final ShowFacade showFacade;
+
+    public ClientShowBookingController(ShowFacade showFacade) {
+        this.showFacade = showFacade;
+    }
+
+    @GetMapping("/booking/{id}")
+    public String seat(@PathVariable("id") long id, Model model){
+        model.addAttribute("show", showFacade.findById(id));
+        return "/pages/client/booking";
+    }
+}
